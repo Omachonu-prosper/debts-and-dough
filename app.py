394720474e691f29fb7934ffc0d119bd2463cff4
Omachonu Prosper <omachonucodes@gmail.com>
2024-10-11 20:59:05 +0100

    Debt page and dough page build

--- app.py
+++ app.py
@@ -4,10 +4,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template(
-        'index.html', title='Home page'
-    )
+def home_page():
+    return render_template('index.html', title='Home page')
+
+
+@app.route('/debts')
+def debts_page():
+    return render_template('debts.html', title='Debts')
+
+
+@app.route('/dough')
+def dough_page():
+    return render_template('dough.html', title='Dough')
 
 
 if __name__ == '__main__':
