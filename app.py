0a9662084f3e8f8c4e6b32ba8d3fbe4b7d607416
Omachonu Prosper <omachonucodes@gmail.com>
2024-10-17 15:32:16 +0100

    Finish up with the signup functionality

--- app.py
+++ app.py
@@ -1,21 +1,63 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, url_for, redirect, flash, session
+from flask_pymongo import PyMongo
+from flask_bcrypt import Bcrypt
+from dotenv import load_dotenv
+from uuid import uuid4
 
 app = Flask(__name__)
+
+load_dotenv(override=True)
+
+app.secret_key = os.getenv('APP_SECRET_KEY')
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+bcrypt = Bcrypt(app) 
+mongo = PyMongo(app)
+
+# Database collections
+users = mongo.db.Users
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        return 'You want to login'
+    else:
+        return render_template('login.html', title='Login')
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup_page():
+    if request.method == 'POST':
+        email = request.form.get('email', None)
+        password = request.form.get('password', None)
+
+        if not email or not password:
+            flash(message="Invalid signup data", category="danger")
+            return redirect(url_for('signup_page'))
+
+        # Check if email is taken
+        if users.find_one({'email': email}):
+            flash(message="A user with the provided email exists", category="warning")
+            return redirect(url_for('signup_page'))
+        
+        password = bcrypt.generate_password_hash(password, 12).decode()
+        user_id = str(uuid4().hex)
+        users.insert_one({
+            'email': email,
+            'password': password,
+            'user_id': user_id
+        })
+        flash(message="Welcome to debts and dough", category="success")
+        session['user_id'] = user_id
+        return redirect(url_for('home_page'))
+    
+    return render_template('signup.html', title='Signup')
 
 
 @app.route('/')
 def home_page():
     return render_template('index.html', title='Home page')
-
-
-@app.route('/login')
-def login_page():
-    return render_template('login.html', title='Login')
-
-
-@app.route('/signup')
-def signup_page():
-    return render_template('signup.html', title='Signup')
 
 
 @app.route('/debts')
