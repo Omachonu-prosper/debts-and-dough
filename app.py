f6a91b8dfc4f64800f0b26691de77a476ffc0b99
Omachonu Prosper <omachonucodes@gmail.com>
2024-10-17 14:25:58 +0100

    Finish up with the front end

--- app.py
+++ app.py
@@ -8,6 +8,16 @@ def home_page():
     return render_template('index.html', title='Home page')
 
 
+@app.route('/login')
+def login_page():
+    return render_template('login.html', title='Login')
+
+
+@app.route('/signup')
+def signup_page():
+    return render_template('signup.html', title='Signup')
+
+
 @app.route('/debts')
 def debts_page():
     return render_template('debts.html', title='Debts')
