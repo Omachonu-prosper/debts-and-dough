23bd372abf06501c4a74e97bd8948e8fcf90dc3f
Omachonu Prosper <omachonucodes@gmail.com>
2024-10-17 18:17:26 +0100

    Mae Increase debtfeature functional

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ def signup_page():
             'email': email,
             'password': password,
             'user_id': user_id,
-            'debts': 0,
+            'debts': 0.0,
             'debts_display': '0.00',
-            'dough': 0,
+            'dough': 0.0,
             'dough_display': '0.00',
             'transactions': [],
             'created_at': timestamp(),
@@ -109,14 +109,59 @@ def home_page():
     return render_template('index.html', title='Home page', user=user)
 
 
-@app.route('/debts')
+@app.route('/debts', methods=['GET', 'POST'])
 def debts_page():
-    return render_template('debts.html', title='Debts')
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('login_page'))
+    
+    user = users.find_one({'user_id': user_id}, {'_id': 0})
+    if not user:
+        session.pop('user_id')
+        return redirect(url_for('login_page'))
+    
+    if request.method == 'POST':
+        increase_debt = request.form.get('increase_debt')
+
+        if increase_debt:
+            # create_transaction_debt(increase_debt, 'increase_debt', users)
+            increase_debt = float(increase_debt)
+            debt_display = user.get('debts') + increase_debt
+            users.update_one(
+                {'user_id': user_id},
+                {'$push': {
+                    'transactions': {
+                        'type': 'debt',
+                        'description': 'Increased Debt',
+                        'amount': increase_debt,
+                        'amount_display': format(increase_debt, ',.2f'),
+                        'created_at': timestamp()
+                    }
+                },
+                '$inc': {
+                    'debts': increase_debt,
+                },
+                '$set': {
+                    'debts_display': format(debt_display, ',.2f')
+                }}
+            )
+
+        return redirect(url_for('debts_page'))         
+     
+    return render_template('debts.html', title='Debts', user=user)
 
 
 @app.route('/dough')
 def dough_page():
-    return render_template('dough.html', title='Dough')
+    user_id = session.get('user_id')
+    if not user_id:
+        return redirect(url_for('login_page'))
+    
+    user = users.find_one({'user_id': user_id}, {'_id': 0})
+    if not user:
+        session.pop('user_id')
+        return redirect(url_for('login_page'))
+    return render_template('dough.html', title='Dough', user=user)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -6,3 +6,10 @@ def timestamp():
     # Returns the time in correspondnce with West African Time
     wat_timezone = timezone('Africa/Lagos')
     return str(datetime.now(wat_timezone))
+
+
+# def create_transaction_debt(amount: float | int, type: str, db):
+#     if type not in ['increase_debt', 'reduce_debt']:
+#         raise Exception('Invalid transaction type')
+    
+#     db.find_one
